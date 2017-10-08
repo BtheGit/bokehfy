b0f54527145e6b8eebadff47b0d6833471e0ac46
BtheGit <brendanwebdev@gmail.com>
2017-10-07 20:13:15 -0600

    Backed out JSDOM

---
+++ src/components/Bokeh.test.js
@@ -0,0 +1,8 @@
+import chai from 'chai';
+import Bokeh from './Bokeh';
+const expect = chai.expect;
+
+describe('Bokeh', function() {
+  it('Should work when jsdom-global stops glitching')
+  it("Won't work because requestanimationframe")
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,41 @@
 const webpack = require('webpack');
+const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const path = require('path');
 const libraryName = 'bokehfy';
-const plugins = [];
 
-const config = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  output: {
-    path: path.resolve(__dirname, 'lib'),
-    filename: libraryName + '.js',
-    library: libraryName,
-    libraryTarget: 'umd',
-    umdNamedDefine: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
+
+module.exports = (env = {}) => {
+  const plugins = [];
+  let outputFile;
+
+  if (env.prod) {
+    plugins.push(new UglifyJsPlugin({ minimize: true }));
+    outputFile = libraryName + '.min.js';
+  } else {
+    outputFile = libraryName + '.js';
+  }  
+
+  return {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+      path: path.resolve(__dirname, 'lib'),
+      filename: outputFile,
+      library: libraryName,
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: [
+            path.resolve(__dirname, 'src')
+          ],
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader'
         }
-      }
-    ]
-  },
-  plugins
-}
-
-module.exports = config;
+      ]
+    },
+    plugins
+  }
+};
