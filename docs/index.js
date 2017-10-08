0c4dd99c642f41fd09daaff53342c30cebf42f75
BtheGit <brendanwebdev@gmail.com>
2017-10-08 00:21:17 -0600

    Demo page

--- docs/index.js
+++ docs/index.js
@@ -1,2 +1,48 @@
 const canvas = document.getElementById('canvas');
 const field = bokehfy(canvas)
+field.recolor('#FFF')
+
+const inFramerate = document.getElementById('in-framerate'),
+      inHalflife = document.getElementById('in-halflife'),
+      inResize = document.getElementById('in-resize'),
+      inDensity = document.getElementById('in-density'),
+      inBackground = document.getElementById('in-background'),
+      inTransparent = document.getElementById('in-transparent'),
+      inColor = document.getElementById('in-color');
+
+const liFramerate = inFramerate.addEventListener('input', changeFramerate),
+      liHalflife = inHalflife.addEventListener('input', changeHalflife),
+      liResize = inResize.addEventListener('input', changeResize),
+      liDensity = inDensity.addEventListener('input', changeDensity),
+      liBackground = inBackground.addEventListener('change', changeBackground),
+      liTransparent = inTransparent.addEventListener('change', changeTransparent),
+      liColor = inColor.addEventListener('change', changeColor);
+
+function changeFramerate(e) {
+  field.framerate(e.target.value)
+}
+
+function changeHalflife(e) {
+  field.halflife(e.target.value)
+}
+
+function changeResize(e) {
+  field.resize(e.target.value)
+}
+
+function changeDensity(e) {
+  field.density(e.target.value)
+}
+
+function changeBackground(e) {
+  console.log(e)
+  field.backgroundColor(e.target.value)
+}
+
+function changeTransparent(e) {
+  field.toggleBackground()
+}
+
+function changeColor(e) {
+  field.recolor(e.target.value)
+}
